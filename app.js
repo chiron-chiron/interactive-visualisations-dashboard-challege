815b5e8795440bb608f00c1cd5930d86e34d7203
chiron-chiron <84137247+chiron-chiron@users.noreply.github.com>
2022-11-27 15:32:13 +1100

    fix errors

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Fetch the JSON data and console log it
 function dropmenu() {
-    d3.json("samepls.json").then((incomingData) => {
+    d3.json("samples.json").then((incomingData) => {
         var data = incomingData;
-        console.log(data)
+        console.log(data) 
         var name = data.names;
         var selDataset = d3.select("#selDataset");
         name.forEach((name) => {
@@ -14,7 +14,7 @@ function dropmenu() {
 
 // Demographic info
 function demoInfo(id) {
-    d3.json("samples").then((incomingData) => {
+    d3.json("samples.json").then((incomingData) => {
         var data = incomingData;
         var metadata = data.metadata;
         var metadataFilter = metadata.filter(x => x.id == id);
@@ -36,7 +36,8 @@ function barchart(id) {
         var sample = data.samples;
         var sampleFilter = sample.filter(x => x.id == id)[0];
         var otuIds = sampleFilter.otu_ids.slice(0, 10).reverse()
-        var otusIdsString = otusIds.map(x => `OTU ${x}`)
+        console.log(otuIds)
+        var otusIdsString = otuIds.map(x => `OTU ${x}`)
         var otuFreq = sampleFilter.sample_values.slice(0, 10).reverse()
         var otuLabels = sampleFilter.otu_labels.slice(0, 10).reverse()
         var barData = [{
@@ -71,7 +72,7 @@ function bubblechart(id) {
             marker: {
                 color: otuIds,
                 size: otuFreq,
-                sizeref: 1.9
+                sizeref: 2.0
             }
         }];
         var layout = {
@@ -83,10 +84,90 @@ function bubblechart(id) {
     })
 }
 
+// Gauge chart
+function gauge(id) {
+    d3.json("samples.json").then((incomingData) => {
+        var data = incomingData;
+        var metadata = data.metadata;
+        var metadataFilter = metadata.filter(x => x.id == id)[0];
+        var wfreq = metadataFilter.wfreq;
+        if (wfreq == null) {
+            wfreq = 0
+        }
+        var gauge = {
+            domain: {x: [0, 1], y: [0, 1]},
+            value: wfreq,
+            type: "indicator",
+            mode: "gauge",
+            gauge: {
+                axis: {
+                    range: [0, 9],
+                    tickmode: "linear",
+                    tickfont: {size: 15}
+                },
+                bar: {color: "rgb(255, 1, 1)"},
+                steps: [
+                    {range: [0,1], color: "rgb(140, 255, 26)" },
+                    {range: [1,2], color: "rgb(153, 255, 51)" },
+                    {range: [2,3], color: "rgb(166, 255, 77)" },
+                    {range: [3,4], color: "rgb(179, 255, 102)" },
+                    {range: [4,5], color: "rgb(191, 255, 128)" },
+                    {range: [5,6], color: "rgb(204, 255, 153)" },
+                    {range: [6,7], color: "rgb(217, 255, 179)" },
+                    {range: [7,8], color: "rgb(230, 255, 204)" },
+                    {range: [8,9], color: "rgb(243, 255, 230)" },
+                ]
+            }
+        };
+        var level = wfreq / 9 * 180;
+        var degrees = 180 - level,
+            radius = 0.7;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+        var mainPath = "M -.0 -0.025 L .0 0.025 L ",
+            pathX = String(x),
+            space = " ",
+            pathY = String(y),
+            pathEnd = " Z";
+        var path = mainPath.concat(pathX, space, pathY, pathEnd);
+        var data = {
+            type: 'scatter',
+            x: [0],
+            y: [0],
+            marker: {size: 12, color: '850000'},
+            showlegend: false,
+            name: wfreq,
+            hoverinfo: "name"
+        }
+        var gaugeData = [gauge, data];
+        var layout = {
+            shapes: [{
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                }
+            }],
+            margin: {t: 0, b: 125, r:50, 1: 50},
+            xaxis: {
+                zeroline: false, showticklabels: false,
+                showgrid: false, range: [-1, 1]
+            },
+            yaxis: {
+                zeroline: false, showticklabels: false,
+                showgrid: false, range: [-0.5, 1.5]
+            }
+        };
+        Plotly.newPlot("gauge", gaugeData, layout);
+    })
+}
+
 var defaultId = 940
 
 function init() {
-    dropdown();
+    dropmenu();
     demoInfo(defaultId);
     barchart(defaultId);
     bubblechart(defaultId);
@@ -101,3 +182,5 @@ function optionChanged(id) {
     bubblechart(id);
     gauge(id);
 }
+
+console.log("test")
